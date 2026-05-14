ce85a03b7dcbef71d97dfa214cacd66016113921
IT21083228 <it21083228@my.sliit.lk>
2026-05-14 23:12:53 +0530

    The folder structure created

---
+++ src/main/java/org/example/fitnessmembershipmanagement/FitnessMembershipManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.fitnessmembershipmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessMembershipManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessMembershipManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fitnessmembershipmanagement/FitnessMembershipManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fitnessmembershipmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessMembershipManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
